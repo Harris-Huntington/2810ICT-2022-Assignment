473caeb5b2f92e15d4e61e31adf039deba5497c7
EricM <eric.martin@griffithuni.edu.au>
2022-10-02 19:09:40 +1000

    jupyter notebook file created

--- data.py
+++ data.py
@@ -1,8 +1,11 @@
+from IPython.display import display
 import pandas as pd
 import re
 
 stats = pd.read_csv('C:/Users/ericm/Documents/SoftwareTech/AssignmentPartB/data/Crash Statistics Victoria.csv', index_col=0)
 code = stats['DCA_CODE'].unique()
+date = stats['ACCIDENT_DATE']
 
 
+print(date)
 print(code, sep="\n", end="\n")
