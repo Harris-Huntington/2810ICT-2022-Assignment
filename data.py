2b310b2c8f115376cbe670fc0439d19690957284
EricM <eric.martin@griffithuni.edu.au>
2022-10-02 17:22:23 +1000

    loaded csv data

--- data.py
+++ data.py
@@ -1,2 +1,8 @@
 import pandas as pd
+import re
 
+stats = pd.read_csv('C:/Users/ericm/Documents/SoftwareTech/AssignmentPartB/data/Crash Statistics Victoria.csv', index_col=0)
+code = stats['DCA_CODE'].unique()
+
+
+print(code, sep="\n", end="\n")
