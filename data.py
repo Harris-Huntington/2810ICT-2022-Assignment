a85de5bb95e041962202fc6ff0021d2472d0596d
EricM <eric.martin@griffithuni.edu.au>
2022-10-06 01:27:53 +1000

    requirements 2 & 3 completed with placeholders

--- data.py
+++ data.py
@@ -7,37 +7,59 @@ import math
 
 #1
 def infoByTime():
-    print(stats[stats["ACCIDENT_DATE"] == "1/7/2013"])
-
+    #print(len(stats.loc[:, ['ACCIDENT_NO','ACCIDENT_DATE','ACCIDENT_TIME','ALCOHOLTIME']][stats["ACCIDENT_DATE"] == "1/7/2013"]))
+    print(stats.loc[:, ['ACCIDENT_NO','ACCIDENT_DATE','ACCIDENT_TIME','ALCOHOLTIME']][stats["ACCIDENT_DATE"] == "1/7/2013"])
 #2
 def accidentByHour():
-    print(stats.loc[:, ['ACCIDENT_TIME']])
-    #xpoints = np.array(stats.loc['ACCIDENT_TIME'])
-    #ypoints = np.array([0,250])
+    date = "3/7/2013"
+    dict1 = {}
+    dict2 = {}
 
-    #plt.plot(xpoints,ypoints)
-    #plt.show()
+    for x in range(len(stats[stats["ACCIDENT_DATE"] == date].iloc[:, 4])):
+
+        if int(str(stats[stats["ACCIDENT_DATE"] == date].iloc[x, 4][0:2])) in dict1:
+            dict1[int(str(stats[stats["ACCIDENT_DATE"] == date].iloc[x, 4][0:2]))] += 1
+        else:
+            dict1.update({int(str(stats[stats["ACCIDENT_DATE"] == date].iloc[x, 4][0:2])): 1})
+
+
+    xpoints = np.array([min(dict1, key=dict1.get),max(dict1, key=dict1.get)])
+    ypoints = np.array([dict1[xpoints[0]], dict1[xpoints[1]]])
+
+    sort = sorted(dict1.items())
+    for i in range(len(dict1)):
+        dict2.update({sort[i][0]:sort[i][1]})
+
+    plt.bar(range(len(dict2)), list(dict2.values()), align='center')
+    plt.xticks(range(len(dict2)), list(dict2.keys()))
+    plt.xlabel("Hour (24hr time)")
+    plt.ylabel("Number of accidents")
+
+    plt.show()
 
 #3
 def keywordByTime():
+    date = '1/7/2013'
+    keyword = 'collision'
+    keyword = keyword.capitalize()
+    type = stats[stats["ACCIDENT_DATE"] == date]["ACCIDENT_TYPE"]
+    
     index = 0
     a = []
     for t in type:
-
-        if re.search("Pedestrian", t) != None:
+        print(re.search(keyword, t))
+        if re.search(keyword, t) != None:
             a.append(index)
         index += 1
-    print(stats.iloc[a,5:9], sep=',')
+    print(stats[stats["ACCIDENT_DATE"] == date].iloc[a,[0,3,4,6]])
 
 
 stats = pd.read_csv('../data/Crash Statistics Victoria.csv', index_col=0)
-code = stats['DCA_CODE'].unique()
-date = stats['ACCIDENT_DATE']
-
-type = stats["ACCIDENT_TYPE"]
 
 
-accidentByHour()
+
+
+keywordByTime()
 
 
 
