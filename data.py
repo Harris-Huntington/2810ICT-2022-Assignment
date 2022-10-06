61800b92abe5061e4674ffcced6b58a26243e6da
EricM <eric.martin@griffithuni.edu.au>
2022-10-06 11:20:44 +1000

    data.py updated

--- data.py
+++ data.py
@@ -3,6 +3,8 @@ import pandas as pd
 import re
 import matplotlib.pyplot as plt
 import numpy as np
+import main
+
 import math
 
 #1
@@ -39,11 +41,11 @@ def accidentByHour():
 
 #3
 def keywordByTime():
-    date = '1/7/2013'
+    date = ''
     keyword = 'collision'
     keyword = keyword.capitalize()
     type = stats[stats["ACCIDENT_DATE"] == date]["ACCIDENT_TYPE"]
-    
+
     index = 0
     a = []
     for t in type:
@@ -53,13 +55,45 @@ def keywordByTime():
         index += 1
     print(stats[stats["ACCIDENT_DATE"] == date].iloc[a,[0,3,4,6]])
 
+#4
+def alcoholAnalysis():
+    date = "3/7/2013"
+
+#5
+def weekdayAnalysis():
+    date = "3/7/2013"
+    dict1 = {}
+    dict2 = {}
+
+    for x in range(len(stats[stats["ACCIDENT_DATE"] == date].iloc[:, 7])):
+
+        if str(stats[stats["ACCIDENT_DATE"] == date].iloc[x, 7][0:2]) in dict1:
+            dict1[str(stats[stats["ACCIDENT_DATE"] == date].iloc[x, 4])] += 1
+        else:
+            dict1.update({str(stats[stats["ACCIDENT_DATE"] == date].iloc[x, 4]): 1})
+
+
+    xpoints = np.array([min(dict1, key=dict1.get),max(dict1, key=dict1.get)])
+    ypoints = np.array([dict1[xpoints[0]], dict1[xpoints[1]]])
+
+    sort = sorted(dict1.items())
+    for i in range(len(dict1)):
+        dict2.update({sort[i][0]:sort[i][1]})
+
+    plt.bar(range(len(dict2)), list(dict2.values()), align='center')
+    plt.xticks(range(len(dict2)), list(dict2.keys()))
+    plt.xlabel("Weekday")
+    plt.ylabel("Number of accidents")
+
+    plt.show()
+
 
 stats = pd.read_csv('../data/Crash Statistics Victoria.csv', index_col=0)
 
 
 
 
-keywordByTime()
+weekdayAnalysis()
 
 
 
@@ -67,5 +101,3 @@ keywordByTime()
 
 
 #[stats[["ACCIDENT_DATE"] == "1/7/2013"]
-
-
