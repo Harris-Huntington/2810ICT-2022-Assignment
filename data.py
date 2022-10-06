9eac42a3387984ee90eafdd50eebd263b6804db2
EricM <eric.martin@griffithuni.edu.au>
2022-10-06 23:31:07 +1000

    integrated data.py and main.py

--- data.py
+++ data.py
@@ -1,29 +1,16 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import re
 import matplotlib.pyplot as plt
 import numpy as np
-
-
 import datetime
 
 
 
-import datetime
-
-
-#import main
-
-
-import math
-
-
-#stats = pd.read_csv('C:/Users/ericm/Documents/SoftwareTech/AssignmentPartB/data/Crash Statistics Victoria.csv', index_col=0)
 
 #1
-def infoByTime():
-    startDate = datetime.datetime.strptime('04/07/2013', '%d/%m/%Y')
-    endDate = datetime.datetime.strptime('19/07/2013', '%d/%m/%Y')
+def infoByTime(sDate, eDate):
+    startDate = datetime.datetime.strptime(sDate, '%d/%m/%Y')
+    endDate = datetime.datetime.strptime(eDate, '%d/%m/%Y')
 
     n = []
     for x in range(len(stats)):
@@ -31,7 +18,6 @@ def infoByTime():
         if startDate <= datetime.datetime.strptime(newdata, '%d/%m/%Y') <= endDate:
             n.append(x)
 
-    #print(len(stats.loc[:, ['ACCIDENT_NO','ACCIDENT_DATE','ACCIDENT_TIME','ALCOHOLTIME']][stats["ACCIDENT_DATE"] == "1/7/2013"]))
     print(stats.iloc[n, [number,date,time,a_type,speed]])
 #2
 
@@ -39,38 +25,24 @@ def accidentByHour(sDate, eDate):
     startDate = datetime.datetime.strptime(sDate, '%d/%m/%Y')
     endDate = datetime.datetime.strptime(eDate, '%d/%m/%Y')
 
-    # startDate = datetime.datetime.strptime('1/7/2013', '%d/%m/%Y')
-    # endDate = datetime.datetime.strptime('5/7/2013', '%d/%m/%Y')
-    print(startDate)
-
-def accidentByHour():
-    startDate = datetime.datetime.strptime('04/07/2013', '%d/%m/%Y')
-    endDate = datetime.datetime.strptime('19/07/2013', '%d/%m/%Y')
-
-
     dict1 = {}
     dict2 = {}
     n = []
-    #stats.query("(ACCIDENT_DATE >= str(startDate) or ACCIDENT_DATE <= str(endDate))")
 
     for x in range(len(stats)):
         newdata = stats.iloc[x, 3]
         if startDate <= datetime.datetime.strptime(newdata, '%d/%m/%Y') <= endDate:
             n.append(x)
 
-    for y in range(len(stats.iloc[n,:])):
-        if stats.iloc[n[y],4][0:2] in dict1:
-            dict1[stats.iloc[n[y],4][0:2]] += 1
+    for y in range(len(stats.iloc[n, :])):
+        if stats.iloc[n[y], 4][0:2] in dict1:
+            dict1[stats.iloc[n[y], 4][0:2]] += 1
         else:
-            dict1.update({stats.iloc[n[y],4][0:2]: 1})
-
+            dict1.update({stats.iloc[n[y], 4][0:2]: 1})
 
     sort = sorted(dict1.items())
     for i in range(len(dict1)):
-        dict2.update({sort[i][0]:sort[i][1]})
-
-    print(dict1)
-    print(dict2)
+        dict2.update({sort[i][0]: sort[i][1]})
 
     plt.bar(range(len(dict2)), list(dict2.values()), align='center')
     plt.xticks(range(len(dict2)), list(dict2.keys()))
@@ -79,10 +51,11 @@ def accidentByHour():
 
     plt.show()
 
+
 #3
-def keywordByTime():
-    startDate = datetime.datetime.strptime('04/07/2013', '%d/%m/%Y')
-    endDate = datetime.datetime.strptime('19/07/2013', '%d/%m/%Y')
+def keywordByTime(sDate, eDate, key):
+    startDate = datetime.datetime.strptime(sDate, '%d/%m/%Y')
+    endDate = datetime.datetime.strptime(eDate, '%d/%m/%Y')
 
     n = []
     for x in range(len(stats)):
@@ -90,7 +63,7 @@ def keywordByTime():
         if startDate <= datetime.datetime.strptime(newdata, '%d/%m/%Y') <= endDate:
             n.append(x)
 
-    keyword = 'pedestrian'
+    keyword = key
     type = stats.iloc[n]["ACCIDENT_TYPE"]
 
     index = 0
@@ -102,34 +75,33 @@ def keywordByTime():
     print(stats.iloc[a,[3,4,6]])
 
 #4
-def alcoholAnalysis():
+def alcoholAnalysis(sDate, eDate):
     date = "3/7/2013"
 
 #5
-def weekdayAnalysis():
-    date = pd.date_range(start = '1/2/2013', end = '10/2/2013')
-    startDate = "3/07/2013"
-    endDate = "9/07/2013"
-    dateRange = ["1/07/2013","2/07/2013","3/07/2013"]
+def weekdayAnalysis(sDate, eDate):
+    startDate = datetime.datetime.strptime('04/07/2013', '%d/%m/%Y')
+    endDate = datetime.datetime.strptime('19/07/2013', '%d/%m/%Y')
 
-    if startDate[1] == '/':
-        if startDate[2:3] == endDate[2:3]:
-            if startDate[0] < endDate[0]:
-                for x in range(0,(int(endDate[0])-int(startDate[0])+1)):
-                    print('j')
-                    dateRange.append(str((x+int(startDate[0])))+"/")
-
+    n = []
+    for x in range(len(stats)):
+        newdata = stats.iloc[x, 3]
+        if startDate <= datetime.datetime.strptime(newdata, '%d/%m/%Y') <= endDate:
+            n.append(x)
 
     dict1 = {}
     dict2 = {}
 
-    for x in range(len(stats[stats["ACCIDENT_DATE"] == date[1]].iloc[:, 7])):
-        print(str(stats[stats["ACCIDENT_DATE"] == date[1]].iloc[x, 7]))
+    for x in range(len(stats.iloc[n])):
+        print(str(stats.iloc[n[x], 7]))
 
-        if str(stats[stats["ACCIDENT_DATE"] == date[1]].iloc[x, 7]) in dict1:
-            dict1[str(stats[stats["ACCIDENT_DATE"] == date[1]].iloc[x, 7])] += 1
+        if str(stats.iloc[n[x], 7]) in dict1:
+            dict1[str(stats.iloc[n[x], 7])] += 1
         else:
-            dict1.update({str(stats[stats["ACCIDENT_DATE"] == date[1]].iloc[x, 4]): 1})
+            if str(stats.iloc[n[x], 7]) == 'nan':
+                ""
+            else:
+                dict1.update({str(stats.iloc[n[x], 7]): 1})
 
     sort = sorted(dict1.items())
     for i in range(len(dict1)):
@@ -140,7 +112,7 @@ def weekdayAnalysis():
     plt.xlabel("Weekday")
     plt.ylabel("Number of accidents")
 
-    #plt.show()
+    plt.show()
 
 
 
@@ -154,12 +126,6 @@ speed = 14
 
 #infoByTime()
 #accidentByHour()
-keywordByTime()
+#keywordByTime()
+#weekdayAnalysis()
 
-
-
-
-#Keyword by timeframe
-
-
-#[stats[["ACCIDENT_DATE"] == "1/7/2013"]
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import wx
+import wx.grid as gridlib
 
 # WX Visuals #
+import data
+
+
 class WindowGUI(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(854,480))
@@ -8,18 +12,20 @@ class WindowGUI(wx.Frame):
 
     def initialise(self):
         pnl = wx.Panel(self)
+        rows = wx.BoxSizer(wx.VERTICAL)
         splitCol = wx.BoxSizer(wx.HORIZONTAL) #Splitting the page
+
         col1 = wx.BoxSizer(wx.VERTICAL) #Left hand col - Selection
         col2 = wx.BoxSizer(wx.VERTICAL) #Right hand col - Title/Data
 
         splitCol.Add(col1, 3, wx.ALIGN_TOP) #Adding main cols
         splitCol.Add(col2, 5, wx.ALIGN_TOP)
 
-
         #First row within each col. Rows follow ABC... formating, Cols 123... etc
         col1A = wx.BoxSizer(wx.HORIZONTAL)
         col1B = wx.BoxSizer(wx.HORIZONTAL)
         col2A = wx.BoxSizer(wx.HORIZONTAL)
+
 
         col1.Add(col1A, 1, wx.ALIGN_CENTER, border=30 ) # Adding rows to main cols
         col1.Add(col1B, 1, wx.ALIGN_CENTER, border=400)
@@ -29,6 +35,15 @@ class WindowGUI(wx.Frame):
         col1A.Add(wx.RadioButton(pnl, 1, 'Alcohol Impacts'), 1)
 
         col2A.Add(wx.StaticText(pnl, 1, label="Victoria State Accident Dataset Analysis"), 1) #Title
+        rows.Add(splitCol, 1, wx.ALIGN_CENTER)
+
+        gridCol = wx.BoxSizer(wx.HORIZONTAL)
+
+        myGrid = gridlib.Grid(pnl)
+        myGrid.CreateGrid(12, 8)
+        gridCol.Add(myGrid, 1, wx.EXPAND)
+        rows.Add(gridCol, 1, wx.ALIGN_LEFT)
+
 
 
         #Selected dates box and controls
@@ -37,11 +52,6 @@ class WindowGUI(wx.Frame):
         wx.StaticBox(pnl, -1, 'Select Dates:', (25,40), size=(280,160))
 
 
-        startDate = wx.TextCtrl(pnl, pos=(60, 70), size=(70, 20))  # Keyword Search
-        startDate.SetHint('1/7/2013') #Oldest Data
-        endDate = wx.TextCtrl(pnl, pos=(200, 70), size=(70, 20))  # Keyword Search
-        endDate.SetHint('21/3/2019') #Newest Date
-
         self.startDate = wx.TextCtrl(pnl, pos=(60, 70), size=(70, 20))  # Keyword Search
         self.startDate.SetHint('xx/xx/xxxx') #Set this to min date
         self.endDate = wx.TextCtrl(pnl, pos=(200, 70), size=(70, 20))  # Keyword Search
@@ -49,10 +59,10 @@ class WindowGUI(wx.Frame):
 
         wx.StaticText(pnl, 1, label="--->", pos=(150,72))
 
-        wx.RadioButton(pnl, 1, 'All', (filterx,filtery),style=wx.RB_GROUP) #Filter Checkboxes
-        wx.RadioButton(pnl, 1, 'Hourly', (2*filterx - 10, filtery))
-        wx.RadioButton(pnl, 1, 'Type', (70, 160))
-        wx.RadioButton(pnl, 1, 'Other', (3*filterx, filtery))
+        self.allR = wx.RadioButton(pnl, 1, 'All', (filterx,filtery),style=wx.RB_GROUP) #Filter Checkboxes
+        self.hourlyR = wx.RadioButton(pnl, 1, 'Hourly', (2*filterx - 10, filtery))
+        self.typeR = wx.RadioButton(pnl, 1, 'Type', (70, 160))
+        self.otherR = wx.RadioButton(pnl, 1, 'Other', (3*filterx, filtery))
 
         self.keyword = wx.TextCtrl(pnl, pos=(120, 160), size=(140, 20))  # Keyword Search
         self.keyword.SetHint('Keyword')
@@ -80,10 +90,28 @@ class WindowGUI(wx.Frame):
 
 
         #Search Button
-        wx.Button(pnl, 1, 'Search', (125,380))
+        searchBtn = wx.Button(pnl, 1, 'Search', (125,380))
+        self.Bind(wx.EVT_BUTTON,self.search, searchBtn)
 
-        pnl.SetSizerAndFit(splitCol)
+
+        pnl.SetSizer(rows)
         self.Show(True)
+        self.Maximize(True)
+
+    def search(self, event):
+        if self.allR.GetValue() == True:
+            data.infoByTime(self.startDate.Value, self.endDate.Value)
+        elif self.hourlyR.GetValue() == True:
+            data.accidentByHour(self.startDate.Value, self.endDate.Value)
+        elif self.typeR.GetValue() == True:
+            data.keywordByTime(self.startDate.Value, self.endDate.Value, self.keyword.Value)
+
+        print(self.startDate.Value, self.endDate.Value)
+
+
+
+
+
 
 app = wx.App()
 frame = WindowGUI(None, "Accident Data Analysis")
